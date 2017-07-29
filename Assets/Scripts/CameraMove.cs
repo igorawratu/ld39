7dd0403d8dd0f773c8e80bcd6c5fb87cf8e833e5
Sunrise Wang <sunrisewng@gmail.com>
2017-07-29 08:28:04 +0200

    initial commit

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+    public GameObject object_to_follow_;
+    public float max_distance_ = 5f;
+    public float desired_height_ = 1f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 curr = new Vector2(gameObject.transform.position.x, gameObject.transform.position.z);
+        Vector2 follow = new Vector2(object_to_follow_.transform.position.x, object_to_follow_.transform.position.z);
+
+        if(Vector2.Distance(curr, follow) > max_distance_)
+        {
+            Vector2 dif = curr - follow;
+            Vector2 desired_p = dif.normalized * max_distance_ + follow;
+            gameObject.transform.position = new Vector3(desired_p.x, 0, desired_p.y);
+        }
+
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, object_to_follow_.transform.position.y + desired_height_, gameObject.transform.position.z);
+        gameObject.transform.LookAt(object_to_follow_.transform);
+	}
+}
---
+++ Assets/cartmovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cartmovement : MonoBehaviour {
+    private Rigidbody rigid_body_;
+	// Use this for initialization
+	void Start () {
+        rigid_body_ = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
